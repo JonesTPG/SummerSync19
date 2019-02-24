5e62b4cb60d4e8ab8727514767e606bf5b4b83d0
joonas <joonas@LAPTOP-HA2MMM9B>
2019-02-24 18:10:50 +0200

    Koodia selkeytetty ja viimeistelty.

--- src/main.java
+++ src/main.java
@@ -18,17 +18,19 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // a small driver program to test out the reservation system.
+        
+        Equipment exampleEq = new Equipment(true, true, 3, false, 2);
         
         
-        PresentationRoom room1 = new PresentationRoom("huone1", "2201", 50, new ArrayList<>());
-        MeetingRoom room2 = new MeetingRoom("huone2", "2203", 15, new ArrayList<>());
+        PresentationRoom room1 = new PresentationRoom("huone1", "2201", 50, exampleEq);
+        MeetingRoom room2 = new MeetingRoom("huone2", "2203", 15, exampleEq);
         
         Customer customer = new PersonalCustomer("Joonas", "050 404 3908", 
         "joonas.ryynanen1@gmail.com");
         
-        Customer customer2 = new PersonalCustomer("Jussi", "050 424 3908", 
-        "jussi.kolehmainen@gmail.com");
+        Customer customer2 = new PersonalCustomer("Jaakko", "050 424 3908", 
+        "jaakko.kolehmainen@gmail.com");
         
         Reservation reservation1 = new Reservation(customer, room1, "24-02-2019",
                 "9:00", "14:00", 5);
@@ -36,8 +38,20 @@ public class main {
         Reservation reservation2 = new Reservation(customer2, room1, "24-02-2019",
                 "12:45", "15:46", 5);
         
+        Reservation reservation3 = new Reservation(customer2, room2, "25-02-2019",
+                "14:45", "15:46", 5);
+        
+        Reservation reservation4 = new Reservation(customer2, room2, "25-02-2019",
+                "16:45", "17:46", 5);
+        
+        Reservation reservation5 = new Reservation(customer2, room2, "25-02-2019",
+                "15:46", "16:00", 5);
+        
         customer.bookReservation(reservation1);
         customer2.bookReservation(reservation2);
+        customer2.bookReservation(reservation3);
+        customer2.bookReservation(reservation4);
+        customer2.bookReservation(reservation5);
         
         
     }
